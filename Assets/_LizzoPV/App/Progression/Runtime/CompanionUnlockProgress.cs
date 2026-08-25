3d6ac746e754ea2e744e2494f124c637723e1a87
lizzo260615-prog <293836385+lizzo260615-prog@users.noreply.github.com>
2026-08-25 16:16:55 +0900

    refactor: isolate companion unlock adapters

--- Assets/_LizzoPV/App/Progression/Runtime/CompanionUnlockProgress.cs
+++ Assets/_LizzoPV/App/Progression/Runtime/CompanionUnlockProgress.cs
@@ -178,38 +178,4 @@ namespace Lizzo.PV.Flow
         static string UnlockResultCountKey(string baseUnitId) => KeyPrefix + "unlock_results." + baseUnitId;
     }
 
-    public sealed class CompanionUnlockProgressRunBinder : IDisposable
-    {
-        readonly CompanionUnlockProgress _progress;
-        readonly RunState _runState;
-        bool _disposed;
-
-        public CompanionUnlockProgressRunBinder(CompanionUnlockProgress progress, RunState runState)
-        {
-            _progress = progress ?? throw new ArgumentNullException(nameof(progress));
-            _runState = runState ?? throw new ArgumentNullException(nameof(runState));
-            _runState.ResultCreated += HandleResultCreated;
-        }
-
-        void HandleResultCreated(RunResult result)
-        {
-            _progress.RecordResultCreated();
-        }
-
-        public void Dispose()
-        {
-            if (_disposed)
-                return;
-
-            _disposed = true;
-            _runState.ResultCreated -= HandleResultCreated;
-        }
-    }
-
-    sealed class PlayerPrefsCompanionUnlockProgressStore : ICompanionUnlockProgressStore
-    {
-        public int GetInt(string key, int defaultValue) => PlayerPrefs.GetInt(key, defaultValue);
-        public void SetInt(string key, int value) => PlayerPrefs.SetInt(key, value);
-        public void Save() => PlayerPrefs.Save();
-    }
 }
