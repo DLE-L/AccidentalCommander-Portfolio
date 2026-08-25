d47884dc2b5afd4b5bef9de240808960b7c30d38
lizzo260615-prog <293836385+lizzo260615-prog@users.noreply.github.com>
2026-08-25 15:53:40 +0900

    refactor: isolate personal summon targeting

--- Assets/_LizzoPV/Gameplay/Combat/Runtime/Summons/CompanionPersonalSummonModule.cs
+++ Assets/_LizzoPV/Gameplay/Combat/Runtime/Summons/CompanionPersonalSummonModule.cs
@@ -43,25 +43,6 @@ namespace Lizzo.PV.Combat.Summons
             && ActiveCap >= 0;
     }
 
-    public readonly struct PersonalSummonTarget
-    {
-        public readonly ICombatImmediateHitTarget Target;
-        public readonly Vector3 Position;
-        public readonly int InstanceId;
-
-        public PersonalSummonTarget(ICombatImmediateHitTarget target, Vector3 position, int instanceId)
-        {
-            Target = target;
-            Position = position;
-            InstanceId = instanceId;
-        }
-    }
-
-    public interface ICompanionPersonalSummonTargetSource
-    {
-        void CollectTargets(List<PersonalSummonTarget> destination);
-    }
-
     public sealed class CompanionPersonalSummonModule : ICompanionPersonalSummonModule
     {
         private readonly IPrefabFactory _factory;
@@ -248,28 +229,4 @@ namespace Lizzo.PV.Combat.Summons
         }
     }
 
-    public sealed class RegistryPersonalSummonTargetSource : ICompanionPersonalSummonTargetSource
-    {
-        private readonly RuntimeObjectRegistry _registry;
-
-        public RegistryPersonalSummonTargetSource(RuntimeObjectRegistry registry)
-        {
-            _registry = registry ?? throw new ArgumentNullException(nameof(registry));
-        }
-
-        public void CollectTargets(List<PersonalSummonTarget> destination)
-        {
-            foreach (MonsterController monster in _registry.Enemies)
-            {
-                if (monster == null || monster.IsValid() == false)
-                    continue;
-
-                Collider2D collider = monster.CombatCollider;
-                Vector3 point = collider == null || collider.enabled == false
-                    ? monster.transform.position
-                    : collider.ClosestPoint(monster.transform.position);
-                destination.Add(new PersonalSummonTarget(monster, point, monster.GetInstanceID()));
-            }
-        }
-    }
 }
